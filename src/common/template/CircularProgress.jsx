4518790bbaa97e1c83319f00893ab87dbf7b8e81
Jeferson Josias Costa <jeferson@sejuf.pr.gov.br>
2022-06-15 17:07:51 -0300

    Adição de circular Progress

---
+++ src/common/template/CircularProgress.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CircularProgress(props) {
+    return(
+    <>
+        <div className="spinner-borderspinner-border text-success" role="status">
+            <span className="visually-hidden">Loading...</span>
+        </div>
+    </>
+    )
+}
+export default CircularProgress
